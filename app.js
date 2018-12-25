28d48edfa8994a29c7915d337b38c9cbad841460
jason <jason@sina.com>
2018-12-25 09:33:39 +0800

    更新代码

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 //创建express的服务器实例
 const app = express()
 
-
+app.use()
 //调用app.listen()方法，指定端口号并启动web服务器
 app.listen(3000, () => {
     console.log('server running at http://127.0.0.1:3000')
